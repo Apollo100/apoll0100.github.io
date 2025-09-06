f3a3158ccad79a61ce4dd0b5ae5f42cec6f4f1f0
Austin <austinpritchard87@gmail.com>
2025-09-06 13:56:34 -0500

    Fixed Styling
    Edited styling of cards and npc strolling to fix some key issues.

--- java/app.js
+++ java/app.js
@@ -1,21 +1,6 @@
-const modal = document.querySelector('#modal');
-const openModal = document.querySelector('.open-button');
-const closeModal = document.querySelector('.close-button');
-
-const themeBtn = document.querySelectorAll('.theme');
-
-const handleThemeChange = (e) => {
-    const theme = e.currentTarget.dataset.theme;
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('customTheme', theme)
-}
-
-themeBtn.forEach(t => t.addEventListener('click', handleThemeChange))
-
-window.addEventListener('DOMContentLoaded', () => {
-    const theme = localStorage.getItem("customTheme");
-    theme && document.documentElement.setAttribute('data-theme', theme);
-})
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.card_button');
+const closeModal = document.querySelector('.close_button');
 
 openModal.addEventListener('click', () => {
     modal.showModal();
@@ -24,4 +9,3 @@ openModal.addEventListener('click', () => {
 closeModal.addEventListener('click', () => {
     modal.close();
 })
-
